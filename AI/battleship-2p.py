63bf86a32e19893c23e5b229b0b12b3fe3d699e1
Shuo Hua <naimen12@gmail.com>
2017-10-26 13:31:57 +0200

    weightcalc was off by 1, pruning could result in empyt target list in 1 specific case

--- AI/battleship-2p.py
+++ AI/battleship-2p.py
@@ -10,13 +10,14 @@ def nextMove(N,grid):
     hit = []
     empty = 0
     adjacent_hit=set()
+    pruned_adjacent_hit = set()
     weights = [[-1 for _ in range(N)] for _ in range(N)]
     for i in range(N):
         for j in range(N):
             if grid[i][j] == 'h':
                 hit.append((i,j))
             elif grid[i][j] == '-':
-                weights[i][j] = calc_weight(i,j,grid)
+                weights[i][j] = calc_weight(i,j,grid)+random()
                 potentialtarget.append((i,j))
                 empty += 1
     
@@ -32,8 +33,12 @@ def nextMove(N,grid):
             if grid[i[0]][i[1]] != '-':
                 adjacent_hit.remove(i)
         if len(hit)>=2:
-            adjacent_hit = prune(hit,adjacent_hit)
-        target = choice(list(adjacent_hit))
+            pruned_adjacent_hit = prune(hit,adjacent_hit) 
+        
+        if list(pruned_adjacent_hit):
+            target = choice(list(pruned_adjacent_hit))
+        else:
+            target = choice(list(adjacent_hit))
     
     elif randrange(empty) < 40 :
         target = choose_max(weights)
@@ -53,15 +58,16 @@ def get_adjacent(x,y,N=10):
 def is_adjacent(a,b):
     return a[0]==b[0] or a[1]==b[1]
 
-def calc_weight(x,y,grid):
+def calc_weight(x,y,grid,N=10):
     weight = 0
     lowx = max(0, min(x-1, N-1))
     highx = max(0, min(x+1, N-1))
     lowy = max(0, min(y-1, N-1))
     highy = max(0, min(y+1, N-1))
+    #logging.debug("({}, {}); ({}, {})".format(lowx,highx,lowy,highy))
     
-    for i in range(lowx,highx):
-        for j in range(lowy,highy):
+    for i in range(lowx,highx+1):
+        for j in range(lowy,highy+1):
             if grid[i][j] == '-':
                 weight += 1
     return weight
